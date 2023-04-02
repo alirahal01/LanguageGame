e6d1991262c1f8f3f0cdd41d8949c5be811cc0b6
ali rahal <ali.rahal01@gmail.com>
2023-04-02 23:40:44 +0300

    Setup

---
+++ WordGame/WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by ali rahal on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
